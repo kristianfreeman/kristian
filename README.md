ab2c5a634728ef2a5ddc31a0cd7ef7386a8c7d40
Kristian Freeman <kristian@kristianfreeman.com>
2024-10-27 15:42:28 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# kristian
+
+## Installation
+
+```sh
+$ npm install kristian
+```
+
+## Usage
+
+```js
+const kristian = require('kristian');
+console.log(kristian.links.github) // https://github.com/kristianfreeman
+```
+
+## Why?
+
+Could be useful.
+
+## License
+
+MIT. Built by [Kristian Freeman](https://kristianfreeman.com).
---
+++ index.js
@@ -0,0 +1,15 @@
+const links = {
+  github: "https://github.com/kristianfreeman",
+  lastfm: "https://last.fm/user/kristianfr",
+  linkedin: "https://www.linkedin.com/in/kristianfreeman/",
+  twitter: "https://x.com/kristianf_",
+  x: "https://x.com/kristianf_",
+  website: "https://kristianfreeman.com",
+};
+
+const kristian = {
+  name: "Kristian Freeman",
+  links,
+};
+
+export default kristian;
